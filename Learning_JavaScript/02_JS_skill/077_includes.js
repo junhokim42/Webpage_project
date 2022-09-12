5e7359a7a10eadca48c2ddbe384bcebc5af7d984
junhokim42 <junhokim42@naver.com>
2022-09-12 22:59:51 +0900

    master

---
+++ Learning_JavaScript/02_JS_skill/077_includes.js
@@ -0,0 +1,11 @@
+const str = 'Make your lives extradordinary';
+
+console.log(str.includes('Make'));
+console.log(str.includes('Make', 1));
+
+console.log('Find Your Own Voice'.toLowerCase());
+console.log('Find Your Own Voice'.toUpperCase());
+
+const value = 'Find Your Own Voice';
+console.log(value.toLowerCase() === value.toUpperCase());
+

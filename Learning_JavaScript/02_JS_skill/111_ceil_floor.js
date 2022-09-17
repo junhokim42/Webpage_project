834cc843693081bfcf4a7118891afdb034c66cfa
CallmeJuno <37017035+junhokim42@users.noreply.github.com>
2022-09-18 01:45:34 +0900

    master

---
+++ Learning_JavaScript/02_JS_skill/111_ceil_floor.js
@@ -0,0 +1,16 @@
+const positiveNum = 93.54;
+const negativeNum = -39.27;
+
+console.log(Math.ceil(positiveNum));
+console.log(Math.ceil(negativeNum));
+console.log(Math.ceil(positiveNum*10)/10);
+console.log(Math.ceil(negativeNum*10)/10);
+console.log(Math.ceil(positiveNum/10)*10);
+console.log(Math.ceil(negativeNum/10)*10);
+
+console.log(Math.floor(positiveNum));
+console.log(Math.floor(negativeNum));
+console.log(Math.floor(positiveNum*10)/10);
+console.log(Math.floor(negativeNum*10)/10);
+console.log(Math.floor(positiveNum/10)*10);
+console.log(Math.floor(negativeNum/10)*10);
